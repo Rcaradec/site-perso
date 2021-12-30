ab6aca3039d9fbaa83a51aa986ca089359c73d07
Rcaradec <rcaradec.dev@gmail.com>
2021-12-30 23:30:51 +0100

    Formation in progress, mapping de fichier data ok /css basique

--- src/components/Formation/index.js
+++ src/components/Formation/index.js
@@ -1,8 +1,43 @@
 import React from "react";
-import { Button } from "../Button";
 import "./Formation.scss";
 import "animate.css";
+import ecoles from "../../data/ecoles";
+
+console.log(ecoles);
 
 export default function Formation() {
-  return <div>FORMATION</div>;
+  return (
+    <div className="formation-container">
+      <div className="formation">
+        <ul className="formation-ul">
+          {ecoles.map((ecole, index) => (
+            <li
+              className="formation-li animate__animated animate__jackInTheBox"
+              key={index}
+            >
+              <a href="#oClock" className="formation-link">
+                {ecole.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div>
+        {ecoles.map((ecole, index) => (
+          <div
+            key={index}
+            className="school-container"
+            style={{ background: ecole.background }}
+          >
+            <h2 className="formation-title " id="oClock">
+              {ecole.title}
+            </h2>
+            <h3 className="formation-subtitle">{ecole.subtitle}</h3>
+            <p className="formation-description">{ecole.description}</p>{" "}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
---
+++ src/data/ecoles.js
@@ -0,0 +1,27 @@
+// Générer un fichier js de datas pour les trois ecoles:
+const ecoles = [
+  {
+    title: "Oclock",
+    subtitle: `On ne va pas à l'école,
+    on y est déjà.`,
+    description:
+      "Des professeurs expérimentés vous forment au métier de développeur, vous accompagnent jusqu'au diplome et vous aident à décrocher un emploi.",
+    background: "lightgray",
+  },
+  {
+    title: "CodeCademy",
+    subtitle: `Join the millions learning to code with Codecademy for free`,
+    description:
+      "Learn the technical skills to get the job you want. Join over 50 million people choosing Codecademy to start a new career (or advance in their current one).",
+    background: "lightsteelblue",
+  },
+  {
+    title: "FreeCodeCamp",
+    subtitle: `Learn to code, for free`,
+    description:
+      "Since 2014, more than 40,000 freeCodeCamp.org graduates have gotten jobs at tech companies ",
+    background: "lightcyan",
+  },
+];
+
+module.exports = ecoles;
